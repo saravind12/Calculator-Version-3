b97fda7dd7eae371189a676f4b893bbfbaf11487
saravind <Home@Home-PC>
2016-02-22 11:37:09 -0700

    Version 1 of Calculator.java

---
+++ cse360assign3/src/cse360assign3/Calculator1Test.java
@@ -0,0 +1,52 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calculator1Test 
+{
+
+	@Test
+	public void testCalculator() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetTotal() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
